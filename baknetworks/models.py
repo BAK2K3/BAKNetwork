9bfdad1f60c42be3a8d84c9894934f4c6e8111b1
bak2k3 <benjamin.a.kavanagh@gmail.com>
2020-07-03 09:35:59 +0100

    Added comments

--- baknetworks/models.py
+++ baknetworks/models.py
@@ -1,41 +1,36 @@
-##BAKNETWORKS MODELS.PY##
-
+###########################
+###BAKNETWORKS MODELS.PY###
+###########################
 
 from datetime import datetime
-from flask_login import UserMixin, current_user, LoginManager
-from flask_dance.consumer.storage.sqla import OAuthConsumerMixin, SQLAlchemyStorage
+from flask_login import UserMixin, LoginManager
+from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
 from flask_sqlalchemy import SQLAlchemy
 
+#Create Database
 db = SQLAlchemy()
 
-
+#User Database
 class User(UserMixin, db.Model):
-
  
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=True)
     picture = db.Column(db.String(200))
-
     posts = db.relationship('Comment', backref='author', lazy=True)
 
     def __repr__(self):
         return f"The current user is {self.name}"
     
-
-    # active = db.Column(db.Boolean, default=False)
-    # tokens = db.Column(db.Text)
-    # created_at = db.Column(db.DateTime, default=datetime.datetime.bstnow())
-
-
+#OAuth Database
 class OAuth(OAuthConsumerMixin, db.Model):
 
     provider_user_id = db.Column(db.String(256), unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     user = db.relationship(User)
 
-    
+ #Comment Database   
 class Comment(db.Model):
 
     __tablename__ = "comments"
@@ -43,10 +38,7 @@ class Comment(db.Model):
     users = db.relationship(User)
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-
     text = db.Column(db.Text, nullable=False)
     page = db.Column(db.String(20), nullable=False)
 
@@ -60,14 +52,7 @@ class Comment(db.Model):
         return f"Post ID: {self.id}"
    
 
-
-## CNNHistory
-## RNNHistory
-## GANHistory
-
-
 login_manager = LoginManager()
-#login_manager.login_view = "google.login"
 
 @login_manager.user_loader
 def load_user(user_id):
