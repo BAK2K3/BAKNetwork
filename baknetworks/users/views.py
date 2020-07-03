de0b6e836d336710aee41faef97628497659188d
bak2k3 <benjamin.a.kavanagh@gmail.com>
2020-07-03 09:44:37 +0100

    Testing required imports

--- baknetworks/users/views.py
+++ baknetworks/users/views.py
@@ -4,25 +4,19 @@
 
 from flask import  redirect, url_for, Blueprint
 from flask_login import login_required, logout_user, current_user
+# from baknetworks import core
 
-from baknetworks import core
-
-from baknetworks.models import User, OAuth, db
-
-from flask_dance.consumer import oauth_authorized, oauth_error
-
-from sqlalchemy.orm.exc import NoResultFound
-
-
-
+# Set up users blueprint
 users = Blueprint('users',__name__)
+
+#Log in route
 @users.route('/google')
 def login():
     if not current_user.is_authenticated:
         return redirect(url_for('google.login'))
-    
     return redirect(url_for('core.index'))
 
+#Log out route
 @users.route('/logout')
 @login_required
 def logout():
