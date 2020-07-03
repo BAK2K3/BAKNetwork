7a94c42b1e2e91fdec998c8451d0e907f02a7fd6
bak2k3 <benjamin.a.kavanagh@gmail.com>
2020-07-03 08:59:26 +0100

    Updated formatting and comments

--- baknetworks/core/views.py
+++ baknetworks/core/views.py
@@ -1,39 +1,44 @@
-##CORE VIEWS.PY##
+####################
+###CORE VIEWS.PY####
+####################
 
 from baknetworks import APP_ROOT
-from flask import render_template, Blueprint, send_file
+from flask import render_template, Blueprint, send_file, redirect
 from flask_login import login_required, current_user
 import os
 
+#Configure blueprint for core routes
 core = Blueprint('core',__name__)
 
-
-
+#Index Routing
 @core.route('/')
 def index():
-
     return render_template('index.html')
 
+#DevDiary Routing
 @core.route('/devdiary')
 def devdiary():
     return render_template('devdiary.html')
 
+#Contact Routing
 @core.route('/contact')
 def contact():
-
     return render_template('contact.html')
 
+#Privacy Page Routing
 @core.route('/privacy')
 def privacy():
-
     return render_template('privacy.html')
 
+#Database Routeing
 @core.route('/database')
 @login_required
 def send_database():
+    #Checks logged in user, and permits download of database if current user is admin.
     if current_user.email == "benjamin.a.kavanagh@gmail.com":
         filename = 'data.sqlite'
         filepath = os.path.join(APP_ROOT, filename)
         return send_file(filepath, as_attachment=True)
+    #Else re-routes to index 
     else:
-        return render_template('index.html')
+        return redirect (url_for('core.index'))
